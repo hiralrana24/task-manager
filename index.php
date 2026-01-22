42177a16aae76ac712afaeb7017fc547eb15b1a7
hiralrana24 <hiral.rana0910@gmail.com>
2026-01-22 10:29:30 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Gestionnaire de tâches</title>
+</head>
+<body>
+
+    <h1>Bienvenue dans le gestionnaire de tâches</h1>
+
+    <p>Choisissez une action :</p>
+
+    <ul>
+        <li><a href="add.php">Ajouter une tâche</a></li>
+        <li><a href="list.php">Voir la liste des tâches</a></li>
+    </ul>
+
+</body>
+</html>
